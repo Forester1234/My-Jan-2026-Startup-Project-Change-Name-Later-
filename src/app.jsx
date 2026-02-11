e936d73f7b7f8e15a18b99b38c96f056bea4e913
Forester1234 <jfc82@byu.edu>
2026-02-11 16:26:59 -0700

    Added Navlinks

--- src/app.jsx
+++ src/app.jsx
@@ -2,37 +2,45 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Scores } from './startgame/startgame';
+import { Play } from './character/character';
+import { About } from './game/game';
+
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent rounded">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="index.html">Digital Dragons</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navMenu">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item"><a className="nav-link" href="index.html">Login</a></li>
-                <li className="nav-item"><a className="nav-link" href="startgame.html">Join Game</a></li>
-                <li className="nav-item"><a className="nav-link" href="character.html">Character Creator</a></li>
-                <li className="nav-item"><a className="nav-link" href="game.html">Game</a></li>
-              </ul>
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light bg-transparent rounded">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="index.html">Digital Dragons</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navMenu">
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item"><NavLink className="nav-link" to="/login">Login</NavLink></li>
+                  <li className="nav-item"><NavLink className="nav-link" to="/startgame">Join Game</NavLink></li>
+                  <li className="nav-item"><NavLink className="nav-link" to="/character">Character Creator</NavLink></li>
+                  <li className="nav-item"><NavLink className="nav-link" to="/game">Game</NavLink></li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </header>
+          </nav>
+        </header>
 
-      <main>App will display here</main>
-      
-      <footer>
-        <hr />
-        <div className="footer-content">
-          <span className="text-reset">Justin Forester Chapman</span>
-          <a href="https://github.com/Forester1234/Foresters-Digital-Dragons-Game" target="_blank" rel="noopener noreferrer">Github</a>
-        </div>
-      </footer>
-    </div>
+        <main>App will display here</main>
+        
+        <footer>
+          <hr />
+          <div className="footer-content">
+            <span className="text-reset">Justin Forester Chapman</span>
+            <a href="https://github.com/Forester1234/Foresters-Digital-Dragons-Game" target="_blank" rel="noopener noreferrer">Github</a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
