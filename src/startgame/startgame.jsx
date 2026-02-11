9f82b82f332dc9315cf4b148d39104d3f5593f46
Forester1234 <jfc82@byu.edu>
2026-02-11 14:22:29 -0700

    Added page specific jsx files

---
+++ src/startgame/startgame.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function StartGame() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Start Game displayed here</div>
+    </main>
+  );
+}
